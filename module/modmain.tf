681de1f418d108fa52eda2224c2f90d3107272b3
Andrew Bobulsky <andrew.bobulsky@buyerquest.com>
2019-07-19 17:57:34 -0400

    Unique subvalue names only

--- module/modmain.tf
+++ module/modmain.tf
@@ -2,12 +2,12 @@ variable "mymap" {
   type        = "map"
   default = {
     key1 = {
-      k1_subkey1 = "k1_subval1"
-      k1_subkey2 = "k1_subval2"
+      subkey1 = "k1_subval1"
+      subkey2 = "k1_subval2"
     },
     key2 = {
-      k2_subkey1 = "k2_subval1"
-      k2_subkey2 = "k2_subval2"
+      subkey1 = "k2_subval1"
+      subkey2 = "k2_subval2"
     }
   }
 }
