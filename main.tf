147d19551464d4cc3ac8b25308843e71ff65d241
Andrew Bobulsky <andrew.bobulsky@buyerquest.com>
2019-07-19 16:52:35 -0400

    Add locals and a data source

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 module "submodule" {
   source = "./module"
 }
+
+# Toplevel key metadata from the submodule output
+locals {
+  toplevel_key_count = "${length(keys(module.submodule.out_mymap))}"
+  toplevel_key_list = "${keys(module.submodule.out_mymap)}"
+}
+
+data "null_data_source" "subkey_enumerator" {
+  count = "${local.toplevel_key_count}"
+  inputs = "${module.submodule.out_mymap[local.toplevel_key_list[count.index]]}"
+}
