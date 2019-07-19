cf463b7abcb0d81219a460dc0a767f35a7a91982
Andrew Bobulsky <andrew.bobulsky@buyerquest.com>
2019-07-19 17:36:24 -0400

    Convert to a list

--- main.tf
+++ main.tf
@@ -2,13 +2,8 @@ module "submodule" {
   source = "./module"
 }
 
-# Toplevel key metadata from the submodule output
-locals {
-  toplevel_key_count = "${length(keys(module.submodule.out_mymap))}"
-  toplevel_key_list = "${keys(module.submodule.out_mymap)}"
-}
-
-data "null_data_source" "subkey_enumerator" {
-  count = "${local.toplevel_key_count}"
-  inputs = "${module.submodule.out_mymap[local.toplevel_key_list[count.index]]}"
+# Turn the subkey data into a list behind a splat
+data "null_data_source" "value_enumerator" {
+  count = "${length(keys(module.submodule.out_mymap))}"
+  inputs = "${module.submodule.out_mymap[element(keys(module.submodule.out_mymap), count.index)]}"
 }
